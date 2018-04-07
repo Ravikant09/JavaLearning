b1637b42284bd766074fee3c8c54d162e0298a13
Ravikant09 <37937959+Ravikant09@users.noreply.github.com>
2018-04-07 14:37:16 +0530

    changes in programm

--- OopsConcept/src/com/oops/Overriding.java
+++ OopsConcept/src/com/oops/Overriding.java
@@ -4,15 +4,22 @@ public class Overriding {
 public static void main(String[] args) {
 	
 	
-	int a;
-	a=1;
-	
-	
-	
-	
-	
-}
-}
-class tai{
-	int b
-}
+	H2 h=new H2();
+		h.add1();
+		
+		
+		
+	}}
+
+	class H1 {
+		void add1() {
+			System.out.println("hello1");
+
+		}
+	}
+
+	class H2 extends H1{
+			void add1(){
+				System.out.println("hello2");
+			}
+		}
